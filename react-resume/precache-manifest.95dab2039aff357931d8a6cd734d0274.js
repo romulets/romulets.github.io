bb31e560f57e4cbdb9e6fb1811662454c4b7a5f4
Romulo Farias <romulo.farias@contabilizei.com.br>
2018-10-25 18:44:04 -0300

    Add react-resume

---
+++ react-resume/precache-manifest.95dab2039aff357931d8a6cd734d0274.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ee4c3ee10a89d4ebe3782b2ff0d9eea",
+    "url": "/static/media/right-arrow.1ee4c3ee.svg"
+  },
+  {
+    "revision": "d51c4cbdf58ed633c094cc393e644e66",
+    "url": "/static/media/down-arrow.d51c4cbd.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6b48b1b9f992d74ba569",
+    "url": "/static/js/main.6b48b1b9.chunk.js"
+  },
+  {
+    "revision": "261d8d98f040a5028c99",
+    "url": "/static/js/1.261d8d98.chunk.js"
+  },
+  {
+    "revision": "6b48b1b9f992d74ba569",
+    "url": "/static/css/main.0d968f62.chunk.css"
+  },
+  {
+    "revision": "273a42db79b0cb94ce86cb2e9e992e26",
+    "url": "/index.html"
+  }
+];
